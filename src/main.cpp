40f25ef9e7325da0bb558fc5b0fbe074511bbb6f
Patrick Burris <nchupacabra@gmail.com>
2025-07-24 15:37:54 -0500

    Added an arena backed vector class

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 
+#include "ArenaVector.hpp"
 #include "FrameArena.hpp"
-#include "Job.hpp"
-#include "JobGraph.hpp"
 #include "ThreadArenaRegistry.hpp"
 
-struct AddIntsNode : JobGraphNode<int, int> {
-  static void run(const int* input, int* output, FrameArena* arena) {
-    *output = *input + 42;
-  }
-};
-
-struct PrintInt : JobGraphNode<int> {
-  static void run(const int* input, FrameArena* arena) {
-    std::cout << "[Print Node]: " << *input << "\n";
-  }
+struct Entity {
+  float x;
+  float y;
+  float z;
+  float velocity;
+  float acceleration;
 };
 
 int main() {
   FrameArena arena(1024);
   ThreadArenaRegistry::set(&arena);
 
-  int input = 5;
-  int* output = arena.allocate<int>();
+  ArenaVector<Entity> ents(&arena, 10);
 
-  Job job = makeJob<AddIntsNode>(&input, output, &arena);
-  Job job2 = makeJob<PrintInt>(output, &arena);
+  for (int i = 0; i < 10; ++i) {
+    float x = i * 10.0f;
+    float y = i * 20.0f;
+    float z = i * 30.0f;
+    float vel = i / 10.0f;
+    float acc = (i * vel);
+    ents.emplace_back(x, y, z, vel, acc);
+  }
 
-  job.fn(job.userData);
-  std::cout << "Job output: " << *output << "\n";
-
-  job2.fn(job2.userData);
+  for (auto& ent : ents) {
+    std::cout << "Entity:\nPosition: <" << ent.x << ", " << ent.y << ", " << ent.z << ">\nVelocity: " << ent.velocity << "\nAcceleration: " << ent.acceleration << "\n";
+  }
 
   ThreadArenaRegistry::clear();
 }
